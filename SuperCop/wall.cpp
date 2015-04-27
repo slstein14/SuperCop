4d5adcfde96e9c6e3dc740d3515b2136e410a6b7
Sparta <dietodeath23@gmail.com>
2015-04-26 22:56:07 -0400

    Updated with changes from merge #16

--- SuperCop/wall.cpp
+++ SuperCop/wall.cpp
@@ -7,7 +7,7 @@ Wall::Wall(QWidget *parent)
 
     gameWindow = parent->width();
     wallPosY = parent->height() - 140;
-    wallPosX = gameWindow + 20;
+    wallPosX = gameWindow + 50;
 
     picX = 32;
     picY = 50;
